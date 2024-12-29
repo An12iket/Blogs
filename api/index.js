4272692eda3fb996f1d6396c9a9583205b95b489
an12iket <aniketchavan121012@gmail.com>
2024-12-29 20:18:28 +0530

    added error handling

--- api/index.js
+++ api/index.js
@@ -1,14 +1,20 @@
 const express = require("express")
 const app = express()
 const cors = require('cors')
+const User = require('./models/User');
 const PORT = 3000 || process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
 
-app.post('/register', (req, res) => {
-    const { username, password } = req.body;
-    res.json({requestData:{username, password}})
+app.post('/register', async (req, res) => {
+    const {username, password} = req.body;
+    try{
+        const userDoc =  await User.create({username,password})
+        res.json(userDoc)
+    } catch(err){
+        res.status(400).json(err)
+    }
 })
 
 
--- api/models/User.js
+++ api/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("")
+mongoose.connect("mongodb+srv://admin:admin%401234@cluster0.82mjkhz.mongodb.net/blog")
 
 const UserSchema = new mongoose.Schema({
     username: {type: String, required: true, min: 4, unique: true},
