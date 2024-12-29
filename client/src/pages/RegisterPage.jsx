08f5f15e12f914ba0701135b98da99395d3a9937
an12iket <aniketchavan121012@gmail.com>
2024-12-29 19:39:55 +0530

    modified form

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const RegisterPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function register(e){
+        e.preventDefault();
+        await fetch('http://localhost:3000/register',{
+            method: 'POST',
+            body: JSON.stringify({
+                username, 
+                password
+            }),
+            headers: {'Content-Type': 'application/json'}
+    })
+    }
+
   return (
     <div>
-      <form className='register'>
+      <form className='register' onSubmit={register}>
         <h1>Register</h1>
-            <input type="text" placeholder='username' required/>
-            <input type="text" placeholder='password' required />
+            <input type="text" 
+            placeholder='username' 
+            value={username} 
+            onChange={(e) => 
+            setUsername(e.target.value)} 
+            required/>
+
+            <input type="password" 
+            placeholder='password' 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            />
             <button>Register</button>
         </form>
     </div>
